c09b6c101b33af92ef9507028e9bc59b3f4f0638
Farhaan Farhaan <ac159962@avcol.school.nz>
2024-05-02 14:06:28 +1200

    few changes made

--- Farhaan/Areas/Identity/Data/FarhaanContext.cs
+++ Farhaan/Areas/Identity/Data/FarhaanContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Farhaan.Models;
+using System.Reflection.Emit;
 
 namespace Farhaan.Areas.Identity.Data;
 
@@ -15,14 +16,27 @@ public class FarhaanContext : IdentityDbContext<IdentityUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
-    }
-
-public DbSet<Farhaan.Models.User> User { get; set; } = default!;
-
-public DbSet<Farhaan.Models.Booking> Booking { get; set; } = default!;
-
-public DbSet<Farhaan.Models.Car> Car { get; set; } = default!;
+    // Customize the ASP.NET Identity model and override the defaults if needed.
+    // For example, you can rename the ASP.NET Identity table names and more.
+    // Add your customizations after calling base.OnModelCreating(builder);
 }
+
+    public DbSet<Farhaan.Models.User> User { get; set; } = default!;
+
+    public DbSet<Farhaan.Models.Booking> Booking { get; set; } = default!;
+
+    public DbSet<Farhaan.Models.Car> Car { get; set; } = default!;
+
+   
+}
+    
+   
+
+
+    
+
+
+
+          
+
+
