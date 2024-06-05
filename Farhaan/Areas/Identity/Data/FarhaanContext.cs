9419aec9a4f764ac5e19d2ebd4f6b3855981b038
Farhaan Farhaan <ac159962@avcol.school.nz>
2024-06-05 15:14:21 +1200

    added booking and cars controllers

--- Farhaan/Areas/Identity/Data/FarhaanContext.cs
+++ Farhaan/Areas/Identity/Data/FarhaanContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Farhaan.Models;
 
 namespace Farhaan.Areas.Identity.Data;
 
@@ -18,4 +19,8 @@ public class FarhaanContext : IdentityDbContext<appUser>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+public DbSet<Farhaan.Models.Booking> Booking { get; set; } = default!;
+
+public DbSet<Farhaan.Models.Car> Car { get; set; } = default!;
 }
